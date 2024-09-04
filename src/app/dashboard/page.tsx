ad5b1556370fc89a3afdd9bb18917fe6245f52da
himalkarkee000 <himalkarkee000@gmail.com>
2024-09-04 12:17:36 +0545

    Routes with pages

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+function BarChart(){
+    return(
+        <>
+        <h1>Bar Chart</h1>
+        </>
+    )
+}
+
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
